9800b025b498f007b419c0e85f45e9a3b7461d3f
GRedex <randula30085@gmail.com>
2025-10-07 11:46:02 +0530

    countryDataset and python connectore created

---
+++ InsertToGenre.sql
@@ -0,0 +1,17 @@
+INSERT INTO Genre VALUES
+('Drama'),
+('Comedy'),
+('Action'),
+('Thriller'),
+('Romance'),
+('Science Fiction'),
+('Horror'),
+('Documentary'),
+('Animation'),
+('Crime'),
+('Adventure'),
+('Fantasy'),
+('Biography'),
+('History'),
+('Musical');
+
