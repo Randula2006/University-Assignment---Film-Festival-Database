dcfc780fe05814c36ddb101b922803bd2a2ef935
GRedex <randula30085@gmail.com>
2025-10-08 12:13:35 +0530

    Add Insertion to country and awards tables

--- dataset_builder/datasets_used/itemizer.py
+++ dataset_builder/datasets_used/itemizer.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv("countryAndCode.csv", encoding="latin1")
 
 for _, row in df.iterrows():
-    print(f"({row['Country']}, {row['Alpha-3 code']}),")
+    print(f"('{row['Country']}', '{row['Alpha-3 code']}'),")
